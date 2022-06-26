0c9697f59e20505f1891c8fbafae8901f76f0e06
TomaszBeben <tbeben@st.swps.edu.pl>
2022-06-26 04:33:21 +0200

    first commit

---
+++ .history/index_20220626020916.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my API')
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`);
+});
